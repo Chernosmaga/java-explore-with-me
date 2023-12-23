b7efa18bdb547f7531061c950044cbb90d5bde58
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-12-23 15:00:00 +0300

    fix: добавил пустой image для main сервиса

--- ewm-stats-service/service/src/main/java/ru/practicum/ExploreWithMeStatisticsService.java
+++ ewm-stats-service/service/src/main/java/ru/practicum/ExploreWithMeStatisticsService.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExploreWithMeService {
+public class ExploreWithMeStatisticsService {
     public static void main(String[] args) {
-        SpringApplication.run(ExploreWithMeService.class, args);
+        SpringApplication.run(ExploreWithMeStatisticsService.class, args);
     }
 }
